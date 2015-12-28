5b08eb2aa7e02a1685f22ab3743e12f5a0394f02
Thomas Ricard <ricard.thomasj@gmail.com>
2015-12-28 18:00:38 -0500

    added easeljs boilerplate

--- js/js.js
+++ js/js.js
@@ -40,3 +40,21 @@ function saveImage(e){
     saveAs(blob, "center-of-mass.png");
   });
 }
+
+var easelElements = {};
+function setupCanvas(){
+    //Create a stage by getting a reference to the canvas
+    stage = new createjs.Stage("demoCanvas");
+    //Create a Shape DisplayObject.
+    circle = new createjs.Shape();
+    circle.graphics.beginFill("red").drawCircle(0, 0, 40);
+    //Set position of Shape instance.
+    circle.x = circle.y = 50;
+    //Add Shape instance to stage display list.
+    stage.addChild(circle);
+    //Update stage will render next frame
+    stage.update();
+    easelElements['stage'] = stage;
+    alert('this ran');
+}
+setupCanvas();
